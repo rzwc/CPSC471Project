9c30b412250dfbdd78e92989164ba1ae90b9a2aa
Leinard Grande <leinard.grande@ucalgary.ca>
2021-12-09 00:22:47 -0700

    fixed some controller errors, reverted back to old program.cs

--- Controllers/ManagersController.cs
+++ Controllers/ManagersController.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using CPSC471Project.Models;
+using RestaurantMS.Models;
 using RestaurantMS.Data;
 
 namespace RestaurantMS.Controllers
