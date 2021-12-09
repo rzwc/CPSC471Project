52ea970336442e35405e23d4bedf36b96b5902ee
Richard <51135299+rzwc@users.noreply.github.com>
2021-12-09 14:59:03 -0700

    include migrations

---
+++ Data/Migrations/20211209214555_FirstMigration.cs
@@ -0,0 +1,234 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RestaurantMS.Data.Migrations
+{
+    public partial class FirstMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Account",
+                columns: table => new
+                {
+                    Account_Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: true),
+                    Permission_Type = table.Column<string>(type: "TEXT", nullable: true),
+                    Password_Hash = table.Column<string>(type: "TEXT", nullable: true),
+                    Phone = table.Column<string>(type: "TEXT", nullable: true),
+                    Address = table.Column<string>(type: "TEXT", nullable: true),
+                    Email = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Account", x => x.Account_Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "DeliveryCar",
+                columns: table => new
+                {
+                    License_plate = table.Column<string>(type: "TEXT", nullable: false),
+                    Mileage = table.Column<int>(type: "INTEGER", nullable: false),
+                    Car_colour = table.Column<string>(type: "TEXT", nullable: true),
+                    Car_model = table.Column<string>(type: "TEXT", nullable: true),
+                    Is_active = table.Column<bool>(type: "INTEGER", nullable: false),
+                    Longitude = table.Column<decimal>(type: "TEXT", nullable: false),
+                    Latitude = table.Column<decimal>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DeliveryCar", x => x.License_plate);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "DrinkItem",
+                columns: table => new
+                {
+                    Drink_ID = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Drink_capacity = table.Column<int>(type: "INTEGER", nullable: false),
+                    Item_ID = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DrinkItem", x => x.Drink_ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Driver",
+                columns: table => new
+                {
+                    Driver_ID = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Drivers_License_Number = table.Column<string>(type: "TEXT", nullable: true),
+                    Account_Id = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Driver", x => x.Driver_ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Employee",
+                columns: table => new
+                {
+                    Employee_Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Wage = table.Column<float>(type: "REAL", nullable: false),
+                    SIN = table.Column<int>(type: "INTEGER", nullable: false),
+                    Position = table.Column<string>(type: "TEXT", nullable: true),
+                    Manager_ID = table.Column<int>(type: "INTEGER", nullable: false),
+                    Account_Id = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employee", x => x.Employee_Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "FoodItem",
+                columns: table => new
+                {
+                    Food_ID = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Drink_capacity = table.Column<int>(type: "INTEGER", nullable: false),
+                    Item_ID = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FoodItem", x => x.Food_ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Ingredients",
+                columns: table => new
+                {
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    Cost = table.Column<float>(type: "REAL", nullable: false),
+                    Calories = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Ingredients", x => x.Name);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Manager",
+                columns: table => new
+                {
+                    Manager_ID = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Account_Id = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Manager", x => x.Manager_ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Menu",
+                columns: table => new
+                {
+                    Menu_Name = table.Column<string>(type: "TEXT", nullable: false),
+                    Last_Updated = table.Column<DateTime>(type: "TEXT", nullable: true),
+                    Category = table.Column<string>(type: "TEXT", nullable: true),
+                    Description = table.Column<string>(type: "TEXT", nullable: true),
+                    Store_ID = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Menu", x => x.Menu_Name);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Order",
+                columns: table => new
+                {
+                    Order_number = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Driver_ID = table.Column<int>(type: "INTEGER", nullable: false),
+                    Customer_ID = table.Column<int>(type: "INTEGER", nullable: false),
+                    Delivery_time = table.Column<string>(type: "TEXT", nullable: true),
+                    Total_cost = table.Column<decimal>(type: "TEXT", nullable: false),
+                    Transaction_time = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Order", x => x.Order_number);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Restaurant",
+                columns: table => new
+                {
+                    Store_ID = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Start_Time = table.Column<string>(type: "TEXT", nullable: true),
+                    End_Time = table.Column<string>(type: "TEXT", nullable: true),
+                    Address = table.Column<string>(type: "TEXT", nullable: true),
+                    Delivery_range = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Restaurant", x => x.Store_ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Review",
+                columns: table => new
+                {
+                    Review_ID = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Comment = table.Column<string>(type: "TEXT", nullable: true),
+                    Rating = table.Column<int>(type: "INTEGER", nullable: false),
+                    Store_ID = table.Column<int>(type: "INTEGER", nullable: false),
+                    Customer_ID = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Review", x => x.Review_ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Account");
+
+            migrationBuilder.DropTable(
+                name: "DeliveryCar");
+
+            migrationBuilder.DropTable(
+                name: "DrinkItem");
+
+            migrationBuilder.DropTable(
+                name: "Driver");
+
+            migrationBuilder.DropTable(
+                name: "Employee");
+
+            migrationBuilder.DropTable(
+                name: "FoodItem");
+
+            migrationBuilder.DropTable(
+                name: "Ingredients");
+
+            migrationBuilder.DropTable(
+                name: "Manager");
+
+            migrationBuilder.DropTable(
+                name: "Menu");
+
+            migrationBuilder.DropTable(
+                name: "Order");
+
+            migrationBuilder.DropTable(
+                name: "Restaurant");
+
+            migrationBuilder.DropTable(
+                name: "Review");
+        }
+    }
+}
